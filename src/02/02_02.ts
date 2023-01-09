5fc953ff329a3ae95742d7a4ef15be2d3e945d20
Artem <reebok001@yandex.ru>
2023-01-10 01:08:48 +0300

    finished 2nd and 3rd lessons

---
+++ src/02/02_02.ts
@@ -0,0 +1,28 @@
+type StreetType = {
+  title: string
+}
+
+type AddressType = {
+  number?: number
+  street: StreetType
+}
+
+type HousesType = {
+  buildedAt: number
+  repaired: boolean
+  address: AddressType
+}
+
+type GovernmentBuildingType = {
+  type: "HOSPITAL" | "FIRE-STATION"
+  budget: number
+  staffCount: number
+  address: AddressType
+}
+
+export type  CityType = {
+  title: string
+  houses: Array<HousesType>
+  governmentBuildings: Array<GovernmentBuildingType>
+  citizensNumber: number
+}
